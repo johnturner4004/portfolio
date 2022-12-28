05609d1cfa70ac5fe981b6e3709c3e57b63de361
John Turner <johnturner4004@gmail.com>
2022-12-28 12:45:13 -0600

    display work history from redux store

--- src/views/WorkHistory.jsx
+++ src/views/WorkHistory.jsx
@@ -1,18 +1,38 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 export default function WorkHistory() {
   const dispatch = useDispatch();
+  const workHistory = useSelector(store => store.workHistory);
+  console.log(workHistory);
 
   useEffect(() => {
-    dispatch({type: 'GET_WORK_HISTORY'})
-  })
+    dispatch({ type: 'GET_WORK_HISTORY' })
+  }, []);
 
   return (
     <div className='work-history'>
       <div className='work-history__monitor'>
         <div className='work-history__monitor-screen'>
-          <h1>This is a test</h1>
+          <h1 className='work-history__company'>Work History<span className='work-history__cursor'>|</span></h1>
+          <ul>
+            {workHistory.data ? workHistory.data.map(entry => {
+              return (
+                <li key={entry._id.toString()}>
+                  <h2>{entry.company}</h2>
+                  <p>{entry.start_date} - {entry.end_date}</p>
+                  <p>{entry.job_title}</p>
+                  <ul>
+                    {entry.job_description ? entry.job_description.map(item => {
+                      return (
+                        <li>{item}</li>
+                      )
+                    }) : ''}
+                  </ul>
+                </li>
+              )
+            }) : ''}
+          </ul>
         </div>
       </div>
     </div>
