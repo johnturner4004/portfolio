014294f3a0d33ba4d2ed1c7d99f95c66114fd69a
John Turner <johnturner4004@gmail.com>
2022-12-31 02:11:09 -0600

    finished form for updating work history

--- src/redux/reducers/work-history.reducer.js
+++ src/redux/reducers/work-history.reducer.js
@@ -1,7 +1,6 @@
 const workHistory = (state = [], action) => {
   switch(action.type) {
     case 'SET_WORK_HISTORY':
-      console.log(action.payload)
       return action.payload;
     default:
       return state;
--- src/views/ManageWorkHistory.jsx
+++ src/views/ManageWorkHistory.jsx
@@ -4,17 +4,57 @@ import Box from '@mui/material/Box';
 import FilledInput from '@mui/material/FilledInput';
 import InputLabel from '@mui/material/InputLabel';
 
-import { useEffect } from "react";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function ManageWorkHistory() {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch({ type: 'GET_WORK_HISTORY' })
   }, [dispatch]);
 
-  const workHistory = useSelector(store => store.workHistory)
+  const workHistory = useSelector(store => store.workHistory);
+  const [change, setChange] = useState();
+
+  const handleChange = (e) => {
+    let tempChange = change
+    if (change && change[e.target.id]) {
+      if (e.target.name === 'job_description') {
+        tempChange[e.target.id].job_description = e.target.value.split('; ');
+      } else {
+        tempChange[e.target.id][e.target.name] = e.target.value
+      }
+    } else {
+      // default:
+      if (e.target.name === 'job_description') {
+        setChange({
+          ...change,
+          [e.target.id]: { [e.target.name]: e.target.value.split('; ') }
+
+        })
+      } else {
+        setChange({
+          ...change,
+          [e.target.id]: {
+            [e.target.name]: e.target.value
+          }
+        })
+      }
+    }
+    console.log(change);
+    //   break;
+    // }
+  }
+
+  const handleSubmit = (e) => {
+    console.log(e.target.dataset.id)
+    if (change && change.hasOwnProperty(e.target.dataset.id)) {
+      console.log(change[e.target.dataset.id]);
+      dispatch({ type: 'UPDATE_WORK_HISTORY', payload: { id: e.target.id, update: change[e.target.dataset.id] }})
+    }
+  }
 
   return (
     <div className='manage-work-history'>
@@ -23,33 +63,39 @@ export default function ManageWorkHistory() {
         {
           workHistory.data && workHistory.data.length > 0 ?
             workHistory.data.map(job => {
+
               return (
-                <li key={job._id}>
-                  <Box className="manage-work-history__box">
+                <li key={job._id} className="manage-work-history__li">
+                  <Box className="manage-work-history__box" mx='auto'>
                     <FormControl sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <TextField name="company" value={job.company} label='Company' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} />
-                      <TextField name="company" value={job.job_title} label='Job Title' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} />
+                      <TextField name="company" defaultValue={job.company} label='Company' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} inputProps={{ 'id': `${job._id}` }} onChange={handleChange} />
+                      <TextField name="job_title" defaultValue={job.job_title} label='Job Title' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} inputProps={{ 'id': `${job._id}` }} onChange={handleChange} />
                     </FormControl>
                     <FormControl sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <TextField name="company" value={job.start_date} label='Start Date' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} />
-                      <TextField name="company" value={job.end_date} label='End Date' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} />
+                      <TextField name="start_date" defaultValue={job.start_date} label='Start Date' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} inputProps={{ 'id': `${job._id}` }} onChange={handleChange} />
+                      <TextField name="end_date" defaultValue={job.end_date} label='End Date' variant="filled" sx={{ mr: 1, mt: 1, mb: 1, width: '100%' }} inputProps={{ 'id': `${job._id}` }} onChange={handleChange} />
                     </FormControl>
                     <FormControl fullWidth sx={{ m: 1, mx: 'auto' }}>
                       <InputLabel variant='filled' shrink>Job Description (separate points with ';')</InputLabel>
                       <FilledInput
                         multiline
                         name="job_description"
-                        value={job.job_description.map(entry => `${entry}; `)}
+                        defaultValue={job.job_description.map(entry => `${entry}; `)}
                         variant="filled"
-                      // onChange={handleChange}
+                        inputProps={{ 'id': `${job._id}` }}
+                        onChange={handleChange}
                       />
+                    </FormControl>
+                    <FormControl className='manage-work-history__row' fullWidth sx={{ mx: 'auto', display: 'flex', flexDirection: 'row' }}>
+                      <Button variant='contained' sx={{ mr: 1, mt: 1, p: 2, width: '50%' }} data-id={job._id} onClick={handleSubmit}>Submit Change</Button>
+                      <Button variant='contained' color='error' sx={{ mt: 1, p: 2, width: '50%' }}>Delete</Button>
                     </FormControl>
                   </Box>
                 </li>
               )
             }) : ''
         }
-      </ul>{JSON.stringify(workHistory.data)}
+      </ul>
     </div>
   )
 }
