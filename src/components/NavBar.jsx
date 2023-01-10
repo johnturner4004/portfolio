df68d43a6f34c62a9e51a72a5448c4fe72780a3f
John Turner <johnturner4004@gmail.com>
2023-01-10 17:20:07 -0600

    replaced button nav with a and span tags:

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,10 +18,10 @@ export default function NavBar() {
         <MenuIcon onClick={handleClick} />
       </div>
       <div className={showMobile ? 'nav-bar__menu' : 'nav-bar__menu hidden-mobile'}>
-        <button className='nav-bar__name' onClick={() => goTo('/')}><h1>John Turner</h1></button>
-        <button className="nav-bar__button" onClick={() => goTo('/work-history')}>Work History</button>
-        <button className="nav-bar__button" onClick={() => goTo('/education')}>Education</button>
-        <button className="nav-bar__button" onClick={() => goTo('/about')}>About</button>
+        <a onClick={() => goTo('/')}><span className='nav-bar__name'>John Turner</span></a>
+        <a onClick={() => goTo('/work-history')}><span className="nav-bar__button">Work History</span></a>
+        <a onClick={() => goTo('/education')}><span className="nav-bar__button">Education</span></a>
+        <a onClick={() => goTo('/about')}><span className="nav-bar__button">About</span></a>
       </div>
     </div>
   )
