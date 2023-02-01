3ec1cccfb646cf5b02e673881b9e7c66399e433b
John Turner <johnturner4004@gmail.com>
2023-02-01 12:53:20 -0600

    build: add amortization schedule

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,6 @@ import { useState } from 'react';
 export default function NavBar() {
   const [showMobile, setShowMobile] = useState(false);
 
-  const goTo = (path) => {
-    window.location.href = path
-  }
-
   const handleClick = () => {
     setShowMobile(!showMobile);
   }
@@ -18,10 +14,10 @@ export default function NavBar() {
         <MenuIcon onClick={handleClick} />
       </div>
       <div className={showMobile ? 'nav-bar__menu' : 'nav-bar__menu hidden-mobile'}>
-        <a onClick={() => goTo('/')}><span className='nav-bar__name'>John Turner</span></a>
-        <a onClick={() => goTo('/work-history')}><span className="nav-bar__button">Work History</span></a>
-        <a onClick={() => goTo('/education')}><span className="nav-bar__button">Education</span></a>
-        <a onClick={() => goTo('/about')}><span className="nav-bar__button">About</span></a>
+        <a href='/'><span className='nav-bar__name'>John Turner</span></a>
+        <a href='/work-history'><span className="nav-bar__button">Work History</span></a>
+        <a href='/education'><span className="nav-bar__button">Education</span></a>
+        <a href='/about'><span className="nav-bar__button">About</span></a>
       </div>
     </div>
   )
--- src/views/Mortgage.jsx
+++ src/views/Mortgage.jsx
@@ -4,6 +4,8 @@ import Button from '@mui/material/Button';
 import Fade from '@mui/material/Fade';
 import { useState } from "react";
 import currency from "currency.js";
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import Grow from '@mui/material/Grow';
 
 export default function Mortgage() {
   const [formState, setFormState] = useState({
@@ -11,7 +13,10 @@ export default function Mortgage() {
     payment: ''
   });
   const [output, setOutput] = useState('empty');
+  const [value, setValue] = useState();
   const [error, setError] = useState({ show: false });
+  const [expand, setExpand] = useState(false);
+  const [schedule, setSchedule] = useState([]);
 
   const handleChange = async (e) => {
     switch (e.target.name) {
@@ -44,39 +49,82 @@ export default function Mortgage() {
 
   const handleClick = () => {
     console.log('Click');
+    const P = currency(formState.amount).value;
+    const A = currency(formState.payment).value;
+    const i = formState.interest / 12 / 100;
+    const n = formState.term * 12;
+
+    let remainingBal, pmt;
+    setSchedule(schedule.length = 0);
+
+    console.log(P + ', ' + A + ', ' + i + ', ' + n);
     switch (true) {
+      // Missing all values
       case !formState.amount && !formState.payment:
         setError({
           show: true,
           message: 'No values entered',
         })
         break;
+      // Missing term and/or interest
       case !formState.term || !formState.interest:
         setError({
           show: true,
           message: 'Term and interest are required fields'
         })
         break;
+      // Used both amount and payment
       case formState.amount !== '' && formState.payment !== '':
         setError({
           show: true,
           message: 'Please only use amount or payment'
         })
         break;
+      // Calculate payment
       case formState.amount !== '':
-        setFormState({
-          ...formState,
-          payment: null
-        })
-        console.log('amount input');
         setOutput('payment');
+        pmt = P * (i * (1 + i) ** n) / ((1 + i) ** n - 1);
+        remainingBal = P;
+        setValue(
+          pmt
+        )
         break;
+      // Calculate total
       case formState.payment !== '':
-        console.log('payment input');
         setOutput('total');
+        pmt = A;
+        remainingBal = A * ((1 + i) ** n - 1) / (i * (1 + i) ** n);
+        setValue(
+          remainingBal
+        )
         break;
+      // Unspecified error
       default:
         console.error('Something went wrong');
+        setError({
+          show: true,
+          message: "I apologize but something went wrong. Please refresh and try again"
+        })
+    }
+
+    for (let k = n; k > 0; k--) {
+      let intPmt = remainingBal * i;
+      let pPmt = pmt - intPmt;
+      remainingBal -= pPmt;
+      let month = n - k + 1
+      setSchedule([
+        schedule.push(
+          {
+            month: month,
+            interest: currency(intPmt).format(),
+            principle: currency(pPmt).format(),
+            remaining: currency(remainingBal).format()
+          }
+        )
+      ])
+      console.log(k, intPmt, pPmt, remainingBal, 'schedule: ');
+      console.log(schedule);
+      setSchedule(schedule);
     }
   }
 
@@ -89,6 +137,7 @@ export default function Mortgage() {
 
   return (
     <div className="mortgage">
+      <NavBar />
       {
         error.show ?
           <Fade in={error.show} timeout={500}>
@@ -103,7 +152,6 @@ export default function Mortgage() {
           </Fade>
           : ''
       }
-      <NavBar />
       <div className="mortgage-calculator">
         <div className="mortgage-input">
           <div className="mortgage-input__row">
@@ -127,13 +175,37 @@ export default function Mortgage() {
                 <span className="mortgage-empty__small">or mortgage total based on payment amount</span>
               </div>
               : output === 'payment' ?
-                <span className="mortgage-payment"><span className="mortgage-payment__amount">{currency(100).format()}</span><br />/month</span>
+                <span className="mortgage-payment"><span className="mortgage-payment__amount">{currency(value).format()}</span><br />/month</span>
                 : output === 'total' ?
-                  <span className="mortgage-total">{currency(100000).format()}</span>
+                  <span className="mortgage-total">{currency(value).format()}</span>
                   : ''
           }
         </div>
+        {
+          output !== 'empty' ?
+            <ExpandCircleDownIcon className="mortgage-button" sx={{ fontSize: 72, color: '#1976d2' }} onClick={() => setExpand(!expand)} />
+            : ''
+        }
       </div>
+      {
+        expand ?
+          <Grow in={expand} style={{ transformOrigin: '0 0 0' }}>
+            <div className="mortgage-schedule">
+              <ul className="mortgage-schedule__list">
+                <li className="mortgage-schedule__list-item"><span>Month</span><span>Interest</span><span>Principle</span><span>Remaining Balance</span></li>
+                {
+                  schedule.map(line => {
+                    return (
+                      <li className="mortgage-schedule__list-item" key={line.month}><span>{line.month}</span><span>{line.interest}</span><span>{line.principle}</span><span>{line.remaining}</span></li>
+                    )
+                  })
+                  // JSON.stringify(schedule)
+                }
+              </ul>
+            </div>
+          </Grow>
+          : ''
+      }
     </div>
   )
 }
