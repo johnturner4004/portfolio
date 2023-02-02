d682aeb2c22a801b5c4dda1548fddf89b83e0bfa
John Turner <johnturner4004@gmail.com>
2023-02-01 18:17:37 -0600

    bug: fixed link thumbnail filling entire page

--- src/views/Sample.jsx
+++ src/views/Sample.jsx
@@ -1,19 +1,24 @@
 import NavBar from '../components/NavBar';
 
 export default function Sample() {
+  const handleClick = (url) => {
+    console.log('click');
+    window.location.href = url;
+  }
+
   return (
     <div className='sample'>
       <NavBar />
       <h1>Sample Projects</h1>
       <div className='sample-page'>
+        {/* <div class="thumbnail-container"> */}
         <a href="/sample/mortgage">
           <h2>Mortgage Calculator</h2>
-          <div class="thumbnail-container">
-            <div class="thumbnail">
-              <iframe src="/sample/mortgage" frameborder="0" title="mortgage calculator thumbnail"></iframe>
-            </div>
-          </div>
         </a>
+        <div className="thumbnail" onClick={() => handleClick('/sample/mortgage')}>
+          <iframe src="/sample/mortgage" frameBorder="0" title="mortgage calculator thumbnail"></iframe>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   )
